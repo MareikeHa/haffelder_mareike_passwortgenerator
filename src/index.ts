e5e67cf8c4081729b3016d42891eab33a5134e82
MareikeHa <mareike.haffelder@gmail.com>
2023-04-26 15:59:57 +0200

    Variablen angelegt

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-//THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+//Variablen anlegen
+
+const spanPasswortanzeige = document.querySelector<HTMLSpanElement>("[data-password]");           //Anzeige von generiertem Passwort + sagen dass Span Element (mit [] genaue Suche nach benennung)
+const form = document.querySelector<HTMLFormElement>("form");                                     //Form
+const inputGroßbuchstaben = document.querySelector<HTMLInputElement>("[data-capital-letters]");   //Großbuchstaben
+const inputZahlen = document.querySelector<HTMLInputElement>("[data-numbers]");                   //Zahlen
+const inputSonderzeichen = document.querySelector<HTMLInputElement>("[data-symbols]");            //Sonderzeichen
+const inputLength = document.querySelector<HTMLInputElement>("[data-length]");                    //Länge
+const buttonNeuesPasswort = document.querySelector<HTMLButtonElement>("[newPassword]");           //Button neues Passwort
+const buttonPasswortDownload = document.querySelector<HTMLButtonElement>("[downloadPassword]");   //Button Passwort herunterladen
