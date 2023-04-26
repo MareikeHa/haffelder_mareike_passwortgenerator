ce5835287cc09380aaf1d0ec551bae2b89e98b42
MareikeHa <mareike.haffelder@gmail.com>
2023-04-26 16:16:58 +0200

    neue .ts Datei, mit Konstanten zur Passwort- Generation

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+//Konstanten/Pool, die für die Generation des Passworts genutzt werden 
+
+export const buchstabenKLEIN: string = "abcdefghijklmnopqrstuvwxyz";
+export const buchstabenGROß: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const NUMMERN: string = "0123456789";
+export const SYMBOLE: string = "!?$%&*@";
