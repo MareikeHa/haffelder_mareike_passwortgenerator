4fb0c8b1b579a4483cf3539ada719c3205ada48b
MareikeHa <mareike.haffelder@gmail.com>
2023-04-28 18:37:20 +0200

    index.ts: Funktion angelegt (Passwort generieren)
    + Variablen Deklaration überarbeitet ("as HTML...Element)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-//Variablen anlegen
+import { NUMMERN, SYMBOLE, buchstabenGROß, buchstabenKLEIN } from "./constants";
 
-const spanPasswortanzeige = document.querySelector<HTMLSpanElement>("[data-password]");           //Anzeige von generiertem Passwort + sagen dass Span Element (mit [] genaue Suche nach benennung)
-const form = document.querySelector<HTMLFormElement>("form");                                     //Form
-const inputGroßbuchstaben = document.querySelector<HTMLInputElement>("[data-capital-letters]");   //Großbuchstaben
-const inputZahlen = document.querySelector<HTMLInputElement>("[data-numbers]");                   //Zahlen
-const inputSonderzeichen = document.querySelector<HTMLInputElement>("[data-symbols]");            //Sonderzeichen
-const inputLänge = document.querySelector<HTMLInputElement>("[data-length]");                     //Länge
-const spanLängenanzeige = document.querySelector<HTMLSpanElement>("[data-current-length]");       //Längenanzeige (Zahl)
-const buttonNeuesPasswort = document.querySelector<HTMLButtonElement>("[newPassword]");           //Button neues Passwort
-const buttonPasswortDownload = document.querySelector<HTMLButtonElement>("[downloadPassword]");   //Button Passwort herunterladen
+//Variablen anlegen
+const spanPasswortanzeige = document.querySelector("[data-password]") as HTMLSpanElement;              //Anzeige von generiertem Passwort + sagen dass Span Element (mit [] genaue Suche nach benennung)
+const form = document.querySelector("form") as HTMLFormElement;                                        //Form
+const inputGroßbuchstaben = document.querySelector("[data-capital-letters]") as HTMLInputElement;      //Großbuchstaben
+const inputZahlen = document.querySelector("[data-numbers]") as HTMLInputElement;                      //Zahlen
+const inputSonderzeichen = document.querySelector("[data-symbols]") as HTMLInputElement;               //Sonderzeichen
+const inputLaenge = document.querySelector("[data-length]") as HTMLInputElement;                       //Länge
+const spanLaengenanzeige = document.querySelector("[data-current-length]") as HTMLSpanElement;         //Längenanzeige (Zahl)
+const buttonNeuesPasswort = document.querySelector("[newPassword]") as HTMLButtonElement;              //Button neues Passwort
+const buttonPasswortDownload = document.querySelector("[downloadPassword]") as HTMLButtonElement;      //Button Passwort herunterladen
+
+function neuesPasswortGenerieren(): string {                        //Funktion generiert neues Passwort
+    let returnable = "";                                            //Passwortstellen
+    const laenge = parseInt(inputLaenge.value);                     //parse.INT um in Zahl zu ändern             
+    const mitGroßbuchstaben = inputGroßbuchstaben.checked;
+    const mitZahlen = inputZahlen.checked;
+    const mitSonderzeichen = inputSonderzeichen.checked;
+    let pool = buchstabenKLEIN;                                     //sind immer enthalten
+    if (mitGroßbuchstaben) pool += buchstabenGROß;                  //falls "mit Großbuchstaben" aktiviert dann zu Pool hinzugefügt
+    if (mitZahlen) pool += NUMMERN;
+    if (mitSonderzeichen) pool += SYMBOLE;
+
+
+    for (let i = 0; i < laenge; i++) {                              //Schleife (läuft solang wie die Passwortlänge)
+                                                                    //in jedem Durchgang wird eine neue Stelle vom Passwort berechnet
+        const random = Math.floor(Math.random() * pool.length);     //neue Random Number (aus 0-9 * Länge von Pool) (math.floor macht eine Ganzzahl draus)
+        returnable += pool[random];                                 //beliebige Stelle im Pool wird gewählt
+    }
+
+    return returnable                                               //gibt die Passwortstellen zurück
+}
+
+console.log(neuesPasswortGenerieren());
